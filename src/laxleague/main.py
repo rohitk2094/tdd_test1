82cd05295dcedb8b9a3c1baa3bab2272d997c485
SEKUMARRO <rohit.kumar2@tetrapak.com>
2021-05-17 16:02:18 +0200

    cleaned repo

---
+++ src/laxleague/main.py
@@ -0,0 +1,9 @@
+def add_two_numbers(a, b):
+    result = a+b
+    return result
+
+if __name__ == "__main__":
+    result = add_two_numbers(5,2)
+    print(result)
+    print('Result is: ', result)
+
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+from laxleague.main import add_two_numbers
+
+
+def test_addition():
+    assert 4 == add_two_numbers(2, 1)
