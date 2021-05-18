c679365cb04c444fcf1e6d07ffcb1de3bf79f588
SEKUMARRO <rohit.kumar2@tetrapak.com>
2021-05-18 13:18:34 +0200

    updated add_two_fractions() and test_addition()

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
-from laxleague.main import add_two_numbers
-
+from laxleague.main import add_two_fractions
 
 def test_addition():
-    assert 4 == add_two_numbers(2, 1)
+    assert (6,8) == add_two_fractions((0,1),(1,2))
+
+
+
