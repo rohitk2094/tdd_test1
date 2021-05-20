df14137fe89af11b3f8516e99884e8a2d196758b
SEKUMARRO <rohit.kumar2@tetrapak.com>
2021-05-20 16:27:21 +0200

    working for one more barcode called 23456

--- src/laxleague/main.py
+++ src/laxleague/main.py
@@ -1,9 +1,85 @@
-def add_two_numbers(a, b):
-    result = a+b
-    return result
+import math
+import numpy as np
+import sys
 
-if __name__ == "__main__":
-    result = add_two_numbers(5,2)
-    print(result)
-    print('Result is: ', result)
 
+# def add_two_fractions(a, b):
+#     # if a[1] is not None
+#     if a[1] == 0 or b[1] == 0:
+#         result = np.nan
+#     elif a[1] is not None and b[1] is not None:
+#         numerator = a[0] * b[1] + b[0] * a[1]
+#         denominator = a[1] * b[1]
+#         result = (numerator, denominator)
+#     return result
+
+# - ----------************  Sell one item **********----------
+N_ITEMS = 10
+
+class Sale:
+
+    # def __init__(self):
+    # def set_priceByBarcode(self, price, barcode):
+    #     self.price_list[barcode] = price
+    #
+    # def get_price(self):
+    #     if barcode in self.price_list:
+    #         price = price_list[barcode]
+
+    def get_price(self, barcode):
+        if barcode == "12345":
+            price = 'EUR: 10'
+        elif barcode == "23456":
+            price = 'EUR: 10.5'
+        else:
+            price = None
+        return price
+
+
+
+
+def generate_price_list(N_ITEMS):
+    price_list = dict()
+    for i in range(1, N_ITEMS + 1):
+        if i < N_ITEMS - 2:
+            price_list[i] = "EUR: " + str(i * 10)
+        else:
+            price_list[i] = None
+
+    print('price_list: \n', price_list)
+    return price_list
+
+
+def get_price(barcode):
+    price_list = generate_price_list(N_ITEMS)
+    barcode = int(barcode)
+    if barcode in price_list:
+        price = price_list[barcode]
+        if price is None:
+            print('No matching price found')
+    elif barcode == "":
+        price = None
+        print('scanning error')
+    else:
+        price = None
+        print('barcode not found')
+    return price
+
+
+N_ITEMS = 10
+price_list = generate_price_list(N_ITEMS)
+
+# # Get the user input on the barcode
+# for j in range(0, 2):
+#     barcode = input('Type barcode: ')
+#     # barcode = '2'
+#     print('barcode: ', barcode)
+#     price = get_price(barcode)
+#     print('price: ', price)
+
+#
+#
+# if __name__ == "__main__":
+#     result = add_two_fractions(5,2)
+#     print(result)
+#     print('Result is: ', result)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,28 @@
-from laxleague.main import add_two_fractions
+import numpy as np
 
-def test_addition():
-    assert (6,8) == add_two_fractions((0,1),(1,2))
+# from laxleague.main import add_two_fractions
 
+# def test_addition():
+#     assert (3, 1) == add_two_fractions((1, 1), (2, 1))
+#     assert (5, 4) == add_two_fractions((3, 2), (6, 4))
+
+# def test_addition_nan():
+#     assert np.nan == add_two_fractions((1, 2), (2, 0))
+
+
+## Sell one item
+from laxleague.main import generate_price_list, get_price, Sale
+
+
+def test_get_price():
+    # assert 50 == get_price('5', price_list)
+    sale = Sale()
+    price = sale.get_price("99999")
+
+    assert price == "Not found"
+
+    # assert 20 == get_price(2, price_list)
+    # assert None == get_price(8, price_list)
+    # assert None == get_price(11, price_list)
 
 
